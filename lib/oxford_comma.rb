4a770dc72d9732821e331fa150907e30dad5ba5f
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 17:41:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    last_string = array.pop
+    array.join(", ")<< ", and #{last_string}"
+  else
+    last_string = array.pop
+    array.join(", ")<< ", and #{last_string}"
+  end
 end
+
