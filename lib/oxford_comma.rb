da8a1be6da17cdfac22c8055e1173b0663dd2a15
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-15 17:40:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-  else
     last_string = array.pop
-    array.join(",")<<  "and #{last_string}"
+    array.join(",")<< "and #{last_string}"
   end
 end
 
